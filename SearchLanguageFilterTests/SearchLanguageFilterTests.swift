8c4fb81bc4cfaf90e838f808124602a76898fedf
Mladen Raykov <raykovmladen@gmail.com>
2025-06-05 11:29:27 +0300

    Initial Commit

---
+++ SearchLanguageFilterTests/SearchLanguageFilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchLanguageFilterTests.swift
+//  SearchLanguageFilterTests
+//
+//  Created by Madji on 5.06.25.
+//
+
+import Testing
+@testable import SearchLanguageFilter
+
+struct SearchLanguageFilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
